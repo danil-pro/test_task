e7ef5bd8b9364af821b84b534961f288f8e87afc
Danil <danil_shevchuk@icloud.com>
2021-05-14 20:35:15 +0300

    update after 10 min

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ BASE_CUR = 'EUR'
 SERVICE_URL = f'http://api.exchangeratesapi.io/v1/latest?base={BASE_CUR}&access_key={ACCESS_KEY}'
 
 DB_PATH = 'db/db.txt'
-DB_CACHE_TIME = 10
+DB_CACHE_TIME = 10 * 60
 
 MESSAGES = {
     'service_error': 'No exchange rate data is available for the selected currency.',
